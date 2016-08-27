0fee9b594ea17308f3380932fc480e551d21a1e9
Lukasz Swiderski <lukasz@swiderski.xyz>
2016-08-27 12:38:28 +0200

    Prepare enum and extension for settings

---
+++ PersonalWebsite/src/PersonalWebsite.Common/Enums/SettingDataType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalWebsite.Common.Enums
+{
+    public enum SettingDataType : byte
+    {
+        LOGIC = 0,
+
+        INT = 1,
+
+        DECIMAL = 2,
+
+        STRING = 3,
+
+        JSON = 4
+    } 
+}
---
+++ PersonalWebsite/src/PersonalWebsite.Common/Extensions/SettingExtension.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace PersonalWebsite.Common.Extensions
+{
+    public static class SettingExtension
+    {
+        public static int FromSetting(this int value, string setting)
+        {
+            var result = int.TryParse(setting, out value);
+
+            if (result)
+            {
+                return value;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static decimal FromSetting(this decimal value, string setting)
+        {
+            var result = decimal.TryParse(setting, out value);
+
+            if (result)
+            {
+                return value;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static bool FromSetting(this bool value, string setting)
+        {
+            var result = bool.TryParse(setting, out value);
+
+            if (result)
+            {
+                return value;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string FromSetting(this string value, string setting)
+        {
+            var result = string.IsNullOrEmpty(setting);
+
+            if (result)
+            {
+                return setting;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+    }
+}
