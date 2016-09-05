73cd14563bd463da350293b4ca7449825af71e42
Lukasz Swiderski <lukasz@swiderski.xyz>
2016-09-05 21:54:18 +0200

    play with css

--- PersonalWebsite/src/PersonalWebsite.Services/Models/Blog/ViewModels/PostViewModel.cs
+++ PersonalWebsite/src/PersonalWebsite.Services/Models/Blog/ViewModels/PostViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,6 +16,7 @@ namespace PersonalWebsite.Services.Models
 
         public string Excerpt { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
         public DateTime CreatedOn { get; set; }
 
         public string Content { get; set; }
