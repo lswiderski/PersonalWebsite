090880096546e6d4df3e959b55c5374796dee4e1
Lukasz Swiderski <lukasz@swiderski.xyz>
2016-09-26 20:03:49 +0200

    Twitter Widget

---
+++ PersonalWebsite/src/PersonalWebsite.Common/Models/TwitterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalWebsite.Common.Models
+{
+    public class TwitterDTO
+    {
+        public string TwitterWidgetId { get; set; }
+
+        public string TwitterUserName { get; set; }
+    }
+}
---
+++ PersonalWebsite/src/PersonalWebsite/Components/TwitterComponent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using PersonalWebsite.Common;
+using PersonalWebsite.Common.Models;
+using PersonalWebsite.Services.Models;
+
+namespace PersonalWebsite.Components
+{
+    public class TwitterComponent : ViewComponent
+    {
+        private readonly ISettingModel settingModel;
+
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public TwitterComponent(ISettingModel settingModel, ICacheService cacheService)
+        {
+            this.settingModel = settingModel;
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+
+            var twitter = new TwitterDTO
+            {
+                TwitterUserName = settingModel.GetString("Twitter.UserName"),
+                TwitterWidgetId = settingModel.GetString("Twitter.Widget.ID")
+            };
+
+            return View(twitter);
+        }
+    }
+}
